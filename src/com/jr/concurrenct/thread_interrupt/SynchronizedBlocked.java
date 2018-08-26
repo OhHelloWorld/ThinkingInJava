12157269efb0fdefc8370e727c6391f0e97f2f5d
OhHelloWorld <18255629607@163.com>
2018-08-26 22:28:11 +0800

    thinking in java

---
+++ src/com/jr/concurrenct/thread_interrupt/SynchronizedBlocked.java
@@ -0,0 +1,25 @@
+package com.jr.concurrenct.thread_interrupt;
+
+public class SynchronizedBlocked implements Runnable {
+
+    public synchronized void f(){
+        while (true){
+            Thread.yield();
+        }
+    }
+
+    public SynchronizedBlocked(){
+        new Thread(){
+            public void run(){
+                f();
+            }
+        }.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Trying to call f()");
+        f();
+        System.out.println("Exiting SynchronizedBlock.run()");
+    }
+}
